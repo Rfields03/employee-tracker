b703783a5b7947c18ea98c1b62da37d24fa289f0
Robert Field <rfields03@gmail.com>
2022-03-15 11:03:39 -0400

    update to code

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 3001,
     user: "root",
     password: "Af983426!@",
     database: "employee_trackerDB"
